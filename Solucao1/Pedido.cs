20d866046f19d72ef308d120c6bba26d8aed9d9e
VINIshow1907 <viniciuslucio792@gmail.com>
2025-03-20 20:47:55 -0300

    :sparkles: Solução 01 terminada 👍

---
+++ Solucao1/Pedido.cs
@@ -0,0 +1,58 @@
+﻿using Solucao1.domains.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solucao1.domains
+{
+    public class Pedido
+    {
+        private StatusPedido statusPedido { get; set; }
+        public Pedido() 
+        {
+            Console.WriteLine("Pedido Aguardando Pagamento");
+            this.statusPedido = StatusPedido.AGUARDANDO_PAGAMENTO;
+        }
+        public void sucessoAoApagar()
+        {
+            if (this.statusPedido == StatusPedido.AGUARDANDO_PAGAMENTO)
+            {
+                //muda o status
+                Console.WriteLine("Pedido Pago");
+                this.statusPedido = StatusPedido.PAGO;
+            }
+            else
+            {
+                throw new Exception("O pedido não está aguardando pagamento");
+            }
+        }
+        public void cancelarPedido()
+        {
+            if (this.statusPedido == StatusPedido.PAGO ||
+                this.statusPedido == StatusPedido.AGUARDANDO_PAGAMENTO)
+            {
+                //muda o status
+                Console.WriteLine("Pedido Cancelado");
+                this.statusPedido = StatusPedido.CANCELADO;
+            }
+            else
+            {
+                throw new Exception("O pedido se encontra " + this.statusPedido.ToString());
+            }      
+        }
+        public void despacharPedido()
+        {
+            if (this.statusPedido == StatusPedido.PAGO) 
+            {
+                //muda o status
+                Console.WriteLine("Pedido Enviado");
+                this.statusPedido = StatusPedido.ENVIADO;
+            } else
+            {
+                throw new Exception("O pedido se encontra cancelado");
+            }
+        }
+    }
+}
---
+++ Solucao1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Solucao1.domains;
+
+try
+{
+    Console.WriteLine("--------------- PEDIDO 01 ---------------");
+    //Faça seus testes aqui!!!
+    Pedido pedido = new Pedido();
+    pedido.sucessoAoApagar();
+    pedido.despacharPedido();
+
+    Console.WriteLine("--------------- PEDIDO 02 ---------------");
+    Pedido pedido02 = new Pedido();
+    pedido02.sucessoAoApagar();
+    pedido02.despacharPedido();
+    pedido02.cancelarPedido();
+
+} catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
