45a5faf591877ee3b821bd158d9b1a391bef7814
VINIshow1907 <viniciuslucio792@gmail.com>
2025-03-20 22:39:54 -0300

    :sparkles: Solução 03 terminada 👍

---
+++ SOLUCOES/Solucao03/Program.cs
@@ -0,0 +1,20 @@
+﻿using Solucao03.domains;
+
+try
+{
+    Console.WriteLine("---------- PEDIDO 01 ----------");
+    //Faça seus testes aqui!!!
+    Pedido pedido = new Pedido();
+    pedido.sucessoAoPagar();
+    pedido.despacharPedido();
+
+    Console.WriteLine("---------- PEDIDO 02 ----------");
+    Pedido pedido2 = new Pedido();
+    pedido2.sucessoAoPagar();
+    pedido2.despacharPedido();
+    pedido2.cancelarPedido();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
---
+++ SOLUCOES/Solucao03/domains/Pedido.cs
@@ -0,0 +1,56 @@
+﻿using Solucao03.domains.state;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solucao03.domains
+{
+    public class Pedido
+    {
+        public State estadoAtual {  get; set; }
+
+        public Pedido() 
+        {
+            Console.WriteLine("Pedido Aguardando Pagamento");
+            //define estado atual
+            this.estadoAtual = new AguardandoPagamentoState(this);
+        }
+        public void sucessoAoPagar()
+        {
+            try
+            {
+                Console.WriteLine("Pedido Pago");
+                this.estadoAtual.sucessoAoPagar();
+            } catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void cancelarPedido()
+        {
+            try
+            {
+                Console.WriteLine("Pedido Cancelado");
+                this.estadoAtual.cancelarPedido();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void despacharPedido()
+        {
+            try
+            {
+                Console.WriteLine("Pedido enviado");
+                this.estadoAtual.despacharPedido();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
