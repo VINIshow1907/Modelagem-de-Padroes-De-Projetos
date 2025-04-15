7d4cc9c3975b9763975a402c1e3dbf356f67f074
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-15 19:47:03 -0300

    :sparkles: feat: Criando TopPagamento

---
+++ ADAPTER/ProjetoAdapter/Pagamento/TopPagamento.cs
@@ -0,0 +1,26 @@
+﻿namespace ProjetoAdapter.Pagamento
+{
+    public class TopPagamento
+    {
+        public void setValorTotal(double valor)
+        {
+            Console.WriteLine($"Valor total definido: R${valor}");
+        }
+
+        public void setCartao(string numeroCartao, string cvv)
+        {
+            Console.WriteLine($"Cartão configurado: {numeroCartao} - CVV: {cvv}");
+        }
+
+        public void setQuantidadeParcelas(int parcelas)
+        {
+            Console.WriteLine($"Parcelado em: {parcelas}x");
+        }
+
+        public bool realizarPagamento()
+        {
+            Console.WriteLine("Pagamento realizado com sucesso via TopPagamento!");
+            return true;
+        }
+    }
+}
