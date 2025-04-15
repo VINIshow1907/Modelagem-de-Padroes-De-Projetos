7462587be91add3750cda84aa87d9cccdd433ab5
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-15 19:46:35 -0300

    :sparkles: feat: Criando pagfacil

---
+++ ADAPTER/ProjetoAdapter/Pagamento/PagFacil.cs
@@ -0,0 +1,33 @@
+﻿namespace ProjetoAdapter.Pagamento
+{
+    public class PagFacil
+    {
+        private double valor;
+        private int parcelas;
+        private string numeroCartao;
+        private string cvv;
+
+        public void configuraPagamento(double valor, int parcelas)
+        {
+            this.valor = valor;
+            this.parcelas = parcelas;
+        }
+
+        public void configuraCartao(string numeroCartao, string cvv)
+        {
+            this.numeroCartao = numeroCartao;
+            this.cvv = cvv;
+        }
+
+        public bool verificaCartao()
+        {
+            return !string.IsNullOrEmpty(numeroCartao) && !string.IsNullOrEmpty(cvv);
+        }
+
+        public bool pagar()
+        {
+            Console.WriteLine($"Pagamento de R${valor} em {parcelas}x realizado via PagFacil.");
+            return true;
+        }
+    }
+}
