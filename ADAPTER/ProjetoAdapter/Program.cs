bf64ca34b7f1e8165b581177fd3501485eef3586
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-15 19:43:59 -0300

    :sparkles: feat: Criando program

---
+++ ADAPTER/ProjetoAdapter/Program.cs
@@ -0,0 +1,20 @@
+﻿using ProjetoAdapter.Adapter;
+using ProjetoAdapter.Cobranca;
+
+Console.WriteLine("Configurando a cobrança");
+Cobranca cobranca = new Cobranca();
+cobranca.setValor(100);
+cobranca.setNumeroCartao("99999999999");
+cobranca.setCVV("163");
+
+
+// Teste PagFacil
+
+cobranca.setGateway(new PagFacilAdapter());
+cobranca.validarCartao();
+cobranca.realizarPagamento();
+
+// Teste TopPagamentos
+cobranca.setGateway(new TopPagamentoAdapter());
+cobranca.validarCartao();
+cobranca.realizarPagamento();
