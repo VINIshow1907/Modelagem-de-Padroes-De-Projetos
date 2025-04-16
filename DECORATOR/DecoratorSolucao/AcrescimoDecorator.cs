ab4e419a7a22e3e9fdb36e020aa699411dec8e03
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-16 07:18:10 -0300

    :sparkles: feat: Criando AcrescimoDecorator

---
+++ DECORATOR/DecoratorSolucao/AcrescimoDecorator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorSolucao.Produto
+{
+    public abstract class AcrescimoDecorator : Pizza
+    {
+        protected Pizza pizza;
+        public abstract override string getDescricao();
+        public abstract override double getPreco();
+    }
+}
