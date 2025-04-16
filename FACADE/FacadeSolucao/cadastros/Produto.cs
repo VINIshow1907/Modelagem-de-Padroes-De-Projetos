14cac3ebfea5446df6d342e65b1d006ec6f937b4
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-16 10:00:21 -0300

    :sparkles: feat: Criando Produto

---
+++ FACADE/FacadeSolucao/cadastros/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeSolucao.cadastros
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, string descricao, double preco)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Preco = preco;
+        }
+    }
+}
+
