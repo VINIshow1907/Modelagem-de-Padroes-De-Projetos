574b939aa3350e71e6beaf1c6da8fdac53c462ea
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-16 10:00:44 -0300

    :sparkles: feat: Criando EmailPedido

---
+++ FACADE/FacadeSolucao/vendas/EmailPedido.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacadeSolucao.cadastros;
+
+namespace FacadeSolucao.vendas
+{
+    public class EmailPedido
+    {
+        private Pedido pedido;
+
+        public EmailPedido(Pedido pedido)
+        {
+            this.pedido = pedido;
+        }
+
+        public void enviarEmail(string mensagem)
+        {
+            Console.WriteLine("Email enviado para: " + pedido.consumidor.Nome);
+            Console.WriteLine(mensagem);
+        }
+    }
+}
+
