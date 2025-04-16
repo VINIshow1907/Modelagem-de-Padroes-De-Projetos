201230e82980269b450ebf9c95c97e47cd9fe8f7
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-16 10:03:01 -0300

    :sparkles: feat: Criando Pedido

---
+++ FACADE/FacadeSolucao/vendas/Pedido.cs
@@ -0,0 +1,23 @@
+﻿using FacadeSolucao.cadastros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeSolucao.vendas
+{
+    public class Pedido
+    {
+        public Consumidor consumidor { get; set; }
+
+        public List<Produto> Produtos { get; set; } = new List<Produto>();
+        public void AddProduto(Produto produto)
+        {
+            Produtos.Add(produto);
+        }
+       
+
+    }
+
+}
