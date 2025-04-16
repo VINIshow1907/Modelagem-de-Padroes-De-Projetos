2015a9def12eb9fd67b45a14df9a954c68a376bf
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-16 10:01:57 -0300

    :sparkles: feat: Criando Pagamento Boleto

---
+++ FACADE/FacadeSolucao/vendas/PagamentoBoleto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FacadeSolucao.vendas
+{
+    public class PagamentoBoleto : Pagamento
+    {
+        private Pedido pedido;
+
+        public PagamentoBoleto(Pedido pedido)
+        {
+            this.pedido = pedido;
+        }
+
+        public override bool realizarPagamento()
+        {
+            return true;
+        }
+    }
+}
+
