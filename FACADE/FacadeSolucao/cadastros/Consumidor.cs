440cdc556d18e0ddf6766db3c1b8decacc4530fd
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-16 10:00:13 -0300

    :sparkles: feat: Criando consumidor

---
+++ FACADE/FacadeSolucao/cadastros/Consumidor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeSolucao.cadastros
+{
+    public class Consumidor
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+
+        public Consumidor(string nome, string email, string cpf)
+        {
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
+    }
+}
+
