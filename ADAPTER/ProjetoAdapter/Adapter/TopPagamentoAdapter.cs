5c864951e27e7773dd46f1ae430d065ce165ef9c
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-22 16:29:40 -0300

    :bug: fix: Configurando TopPagamentoAdapter corretamente

--- ADAPTER/ProjetoAdapter/Adapter/TopPagamentoAdapter.cs
+++ ADAPTER/ProjetoAdapter/Adapter/TopPagamentoAdapter.cs
@@ -1,49 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ProjetoAdapter.Pagamento;
+﻿using ProjetoAdapter.Pagamento;
 
 namespace ProjetoAdapter.Adapter
 {
     public class TopPagamentoAdapter : Gateway
     {
         public TopPagamento topPagamentos;
-        public string numeroCartao {  get; set; }
-        public string cvv {  get; set; }
-        public double valor {  get; set; }
+        public string numeroCartao { get; set; }
+        public string cvv { get; set; }
+        public double valor { get; set; }
         public int parcelas { get; set; }
 
-        public TopPagamentoAdapter() {
+        public TopPagamentoAdapter()
+        {
             this.topPagamentos = new TopPagamento();
         }
+
         public void setCVV(string cvv)
         {
             this.cvv = cvv;
         }
+
         public void setNumeroCartao(string numeroCartao)
         {
             this.numeroCartao = numeroCartao;
         }
+
         public void setParcelas(int parcelas)
         {
             this.parcelas = parcelas;
         }
+
         public void setValor(double valor)
         {
             this.valor = valor;
         }
+
         public bool validarCartao()
         {
+            Console.WriteLine("Cartão validado com sucesso");
             return true;
         }
+
         public bool realizarPagamento()
         {
             this.topPagamentos.setValorTotal(this.valor);
             this.topPagamentos.setCartao(this.numeroCartao, this.cvv);
             this.topPagamentos.setQuantidadeParcelas(this.parcelas);
-
+            Console.WriteLine("Aprovado");
             return this.topPagamentos.realizarPagamento();
         }
     }
