7b2d956d1d4c1a510ef17beaaf9233cfa6830b4c
VINIshow1907 <viniciuslucio792@gmail.com>
2025-04-22 16:30:27 -0300

    :bug: fix: Configurando TopPagamento corretamente

--- ADAPTER/ProjetoAdapter/Pagamento/TopPagamento.cs
+++ ADAPTER/ProjetoAdapter/Pagamento/TopPagamento.cs
@@ -4,22 +4,22 @@
     {
         public void setValorTotal(double valor)
         {
-            Console.WriteLine($"Valor total definido: R${valor}");
+           
         }
 
         public void setCartao(string numeroCartao, string cvv)
         {
-            Console.WriteLine($"Cartão configurado: {numeroCartao} - CVV: {cvv}");
+           
         }
 
         public void setQuantidadeParcelas(int parcelas)
         {
-            Console.WriteLine($"Parcelado em: {parcelas}x");
+           
         }
 
         public bool realizarPagamento()
         {
-            Console.WriteLine("Pagamento realizado com sucesso via TopPagamento!");
+            
             return true;
         }
     }
