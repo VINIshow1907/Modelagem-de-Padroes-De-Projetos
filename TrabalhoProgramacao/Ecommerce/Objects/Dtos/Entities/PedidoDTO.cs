488a1a79c14474be49eed4728063d3065f3f5248
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-15 13:29:40 -0300

    :sparkles: feat: Criando Objects -> Dtos -> Entities -> PedidoDTO

---
+++ TrabalhoProgramacao/Ecommerce/Objects/Dtos/Entities/PedidoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.Objects.Dtos.Entities
+{
+    public class PedidoDTO
+    {
+    public int Id { get; set; }
+    public string Descricao { get; set;}
+    public float Valor {  get; set;}
+
+    }
+}
