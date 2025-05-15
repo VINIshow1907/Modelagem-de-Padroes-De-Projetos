4274f105bf0fc619d7530a4f780e483c052f82e9
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-15 13:33:02 -0300

    :sparkles: feat: Criando State Cancelado

---
+++ TrabalhoProgramacao/Ecommerce/Service/State/CanceladoState.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Objects.Models;
+
+namespace Ecommerce.Service.State
+{
+    public class CanceladoState : IState
+    {
+        private Pedido pedido;
+        public CanceladoState(Pedido pedido)
+        {
+            this.pedido = pedido;
+        }
+        void IState.cancelarPedido()
+        {
+            throw new Exception("Operação não suportada, pedido foi cancelado");
+        }
+        void IState.despacharPedido()
+        {
+            throw new Exception("Operação não suportada, pedido foi cancelado");
+        }
+        void IState.sucessoAoPagar()
+        {
+            throw new Exception("Operação não suportada, pedido foi cancelado");
+        }
+    }
+}
