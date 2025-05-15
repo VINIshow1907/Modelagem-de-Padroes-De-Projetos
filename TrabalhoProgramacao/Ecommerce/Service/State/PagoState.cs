6521918bf12320d8dd4fd5eecdd90234960b5307
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-15 13:33:41 -0300

    :sparkles: feat: Criando State Pago

---
+++ TrabalhoProgramacao/Ecommerce/Service/State/PagoState.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Objects.Models;
+
+namespace Ecommerce.Service.State
+{
+    public class PagoState : IState
+    {
+        private Pedido pedido;
+        public PagoState(Pedido pedido)
+        {
+            this.pedido = pedido;
+        }
+        void IState.cancelarPedido()
+        {
+            this.pedido.EstadoAtual = new CanceladoState(pedido);
+        }
+        void IState.despacharPedido()
+        {
+            this.pedido.EstadoAtual = new EnviadoState(pedido);
+        }
+        void IState.sucessoAoPagar()
+        {
+            throw new Exception("Operação não suportada, pedido já foi pago");
+        }
+    }
+}
