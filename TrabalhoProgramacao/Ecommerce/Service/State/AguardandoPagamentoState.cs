d90a4a662cc281073304d0876e8ea8b96ce48b73
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-15 13:32:47 -0300

    :sparkles: feat: Criando State AguardandoPagamento

---
+++ TrabalhoProgramacao/Ecommerce/Service/State/AguardandoPagamentoState.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.Objects.Models;
+
+namespace Ecommerce.Service.State
+{
+    public class AguardandoPagamentoState : IState
+    {
+        private Pedido pedido;
+
+        public AguardandoPagamentoState(Pedido pedido)
+        {
+            this.pedido = pedido;
+        }
+        void IState.cancelarPedido()
+        {
+            this.pedido.EstadoAtual = new CanceladoState(pedido);
+        }
+        void IState.despacharPedido()
+        {
+            throw new Exception("Operação não suportada, " + "pedido ainda não foi pago");
+        }
+        void IState.sucessoAoPagar()
+        {
+            this.pedido.EstadoAtual = new PagoState(pedido);
+        }
+    }
+}
