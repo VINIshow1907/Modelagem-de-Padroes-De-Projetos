9d59d77c601f8e962d18907a577a7d9f8d65d48e
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-15 13:28:29 -0300

    :sparkles: Criando Models -> Pedido

---
+++ TrabalhoProgramacao/Ecommerce/Objects/Models/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.Objects.Models;
+
+[Table("pedido")]
+public class Pedido
+{
+    [Column("id")]
+    public int Id { get; set; }
+    
+    [Column("descricao")]
+    public string Descricao { get; set; }
+
+    [Column("valor")]
+    public float Valor {  get; set; }
+}
