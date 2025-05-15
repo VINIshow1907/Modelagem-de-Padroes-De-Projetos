7a59757859f7738993b3638752c2a89fc0fc1c07
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-15 13:33:16 -0300

    :sparkles: feat: Criando State Enviado

---
+++ TrabalhoProgramacao/Ecommerce/Service/State/EnviadoState.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Objects.Models;
+
+namespace Ecommerce.Service.State
+{
+    public class EnviadoState : IState
+    {
+        private Pedido pedido;
+        public EnviadoState(Pedido pedido)
+        {
+            this.pedido = pedido;
+        }
+        void IState.cancelarPedido()
+        {
+            throw new Exception("Operação não suportada, pedido já foi enviado");
+        }
+        void IState.despacharPedido()
+        {
+            throw new Exception("Operação não suportada, pedido já foi enviado");
+        }
+        void IState.sucessoAoPagar()
+        {
+            throw new Exception("Operação não suportada, pedido já foi enviado");
+        }
+    }
+}
