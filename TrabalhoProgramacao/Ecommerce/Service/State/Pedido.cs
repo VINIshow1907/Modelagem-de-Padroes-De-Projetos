640d4876845dbb79083b0e21000c2a9fe99f1bee
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-15 13:33:51 -0300

    :sparkles: feat: Criando State Pedido

---
+++ TrabalhoProgramacao/Ecommerce/Service/State/Pedido.cs
@@ -0,0 +1,27 @@
+﻿namespace Ecommerce.Service.State
+{
+    public class Pedido
+    {
+        public IState EstadoAtual { get; set; }
+
+        public Pedido()
+        {
+            EstadoAtual = new AguardandoPagamentoState(this);
+        }
+
+        public void Pagar()
+        {
+            EstadoAtual.sucessoAoPagar();
+        }
+
+        public void Cancelar()
+        {
+            EstadoAtual.cancelarPedido();
+        }
+
+        public void Enviar()
+        {
+            EstadoAtual.despacharPedido();
+        }
+    }
+}
