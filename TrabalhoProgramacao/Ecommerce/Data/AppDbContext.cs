b24ff448e0833fdc6adbe99cf897c9983d0b3950
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-16 14:29:21 -0300

    :sparkles: feat: Criando AppDbContext

---
+++ TrabalhoProgramacao/Ecommerce/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Data.Builders;
+using Ecommerce.Objects.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+
+namespace Ecommerce.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Pedido> Pedido { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            PedidoBuilder.Build(modelBuilder);
+
+        }
+    }
+}
