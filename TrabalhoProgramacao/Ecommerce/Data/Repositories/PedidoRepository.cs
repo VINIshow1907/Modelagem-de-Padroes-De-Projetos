1d119d2a8a87bfe747b6023f502fa91099428312
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-16 14:28:55 -0300

    :sparkles: feat: Criando PedidoRepository

---
+++ TrabalhoProgramacao/Ecommerce/Data/Repositories/PedidoRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Data.Interfaces;
+using Ecommerce.Objects.Models;
+using System.Threading;
+
+namespace Ecommerce.Data.Repositories
+{
+    public class PedidoRepository : GenericRepository<Pedido>, IPedidoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PedidoRepository(AppDbContext context) : base(context)
+        {
+            this._context = context;
+        }
+    }
+}
