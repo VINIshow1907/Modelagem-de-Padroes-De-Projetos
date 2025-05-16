33a998ffd5f98d82be7619ee1cb0fe535fa10c90
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-16 14:24:42 -0300

    :refactor: refactor: Classes apagadas

--- TrabalhoProgramacao/Ecommerce/Service/State/IState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ecommerce.Service.State
-{
-    public interface IState
-    {
-        public void sucessoAoPagar();
-        public void cancelarPedido();
-        public void despacharPedido();
-    }
-}
