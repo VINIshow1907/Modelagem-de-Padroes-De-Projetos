49f8ce0a0737bc767abdfeff874a921197f0c18d
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-16 14:27:58 -0300

    sparkles: Criando IPedidoRepository

---
+++ TrabalhoProgramacao/Ecommerce/Data/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Ecommerce.Objects.Models;
+using System.Threading;
+
+namespace Ecommerce.Data.Interfaces
+{
+    public interface IPedidoRepository : IGenericRepository<Pedido>
+    {
+
+    }
+}
