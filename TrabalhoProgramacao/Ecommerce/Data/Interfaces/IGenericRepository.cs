34f547e7adf33683b66abaa343567f47d779e8e5
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-16 14:27:34 -0300

    :sparkles: feat: Criando IGenericRepository

---
+++ TrabalhoProgramacao/Ecommerce/Data/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Objects.Models;
+
+namespace Ecommerce.Data.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> Get();
+        Task<T> GetById(int id);
+        Task GerarPedido(T entity);
+        Task Update(T entity);
+        Task<bool> SaveChanges();
+    }
+}
