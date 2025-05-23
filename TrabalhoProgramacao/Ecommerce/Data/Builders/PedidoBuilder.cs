67c9343c5ad8ee7a14e96eb79df7721513dec05b
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-22 22:02:20 -0300

    :recycle: refactor: Arrumando Chamadas

--- TrabalhoProgramacao/Ecommerce/Data/Builders/PedidoBuilder.cs
+++ TrabalhoProgramacao/Ecommerce/Data/Builders/PedidoBuilder.cs
@@ -11,19 +11,19 @@ namespace Ecommerce.Data.Builders
         public static void Build(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Pedido>().HasKey(pg => pg.Id);
-            modelBuilder.Entity<Pedido>().Property(pg => pg.DescricaoProduto)
+            modelBuilder.Entity<Pedido>().Property(pg => pg.Descricao)
                 .IsRequired().HasMaxLength(100);
-            modelBuilder.Entity<Pedido>().Property(pg => pg.ValorProduto)
+            modelBuilder.Entity<Pedido>().Property(pg => pg.Valor)
                 .IsRequired().HasMaxLength(50);
-            modelBuilder.Entity<Pedido>().Property(pg => pg.ValorFrete)
+            modelBuilder.Entity<Pedido>().Property(pg => pg.TotalGeral)
                .IsRequired().HasMaxLength(50);
 
             modelBuilder.Entity<Pedido>()
                 .HasData(new List<Pedido>
                 {
-                new Pedido(1, "Camisa Real Madrid", (float)20.5, (float)2.5, PedidoType.PAGO, FreteType.Terrestre),
-                new Pedido(2, "Camisa Barcelona", (float)50.50, (float)25.5, PedidoType.PAGO, FreteType.Terrestre),
-                new Pedido(3, "Camisa Corinthians", (float)100.00, (float)25.5, PedidoType.PAGO, FreteType.Terrestre),
+                new Pedido(1, "Camisa Real Madrid", (float)20.5, (float)2.5, PedidoType.Pago, FreteType.Terrestre),
+                new Pedido(2, "Camisa Barcelona", (float)50.50, (float)25.5, PedidoType.Pago, FreteType.Terrestre),
+                new Pedido(3, "Camisa Corinthians", (float)100.00, (float)25.5, PedidoType.Pago, FreteType.Terrestre),
                 });
         }
     }
