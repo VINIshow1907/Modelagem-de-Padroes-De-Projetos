1cf6fba65796e8cfe093d2a12b074c2a42f9e611
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-22 22:14:34 -0300

    :sparkles: feat: Arrumando PedidoDTO

--- TrabalhoProgramacao/Ecommerce/Objects/Dtos/Entities/PedidoDTO.cs
+++ TrabalhoProgramacao/Ecommerce/Objects/Dtos/Entities/PedidoDTO.cs
@@ -5,6 +5,9 @@
     public int Id { get; set; }
     public string Descricao { get; set;}
     public float Valor {  get; set;}
+    public double TotalGeral { get; set; }
+    public int PedidoType { get; set; }
+    public int FreteType { get; set; }
 
     }
 }
