33448aece8a612f786f5189c24a9f5e0f751f341
VINIshow1907 <viniciuslucio792@gmail.com>
2025-06-15 14:18:19 -0300

    :sparkles: feat: AbstractFactory concluido

---
+++ ABSTRACT FACTORY/AbstractFactorySolucao/Boleto/Familia/CalculosFactory.cs
@@ -0,0 +1,16 @@
+﻿using AbstractFactorySolucao.Boleto.ConfiguraDesconto;
+using AbstractFactorySolucao.Boleto.ConfiguraJuros;
+using AbstractFactorySolucao.Boleto.ConfiguraMulta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactorySolucao.Boleto.Familia;
+public interface CalculosFactory
+{
+    public Juros criarJuros();
+    public Desconto criarDesconto();
+    public Multa criarMulta();
+}
--- ABSTRACT FACTORY/AbstractFactorySolucao/Program.cs
+++ ABSTRACT FACTORY/AbstractFactorySolucao/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using AbstractFactorySolucao.Boleto;
+using AbstractFactorySolucao.Boleto.Familia;
+
+Banco banco = new Banco();
+
+CalculosFactory factoryCaixa = new CaixaCalculosFactory();
+
+CalculosFactory factoryBB = new BBCalculosFactory();
+
+Console.WriteLine(" criar boleto caixa");
+banco.gerarBoleto(100,factoryCaixa);
+
+Console.WriteLine("criar boleto BB");
+banco.gerarBoleto(100, factoryBB);
